6f722aa50da3aeaa55e7c97deaff5eea601d4f12
gokselkucuksahin <gokselkucuksahin@gmail.com>
2022-09-04 21:28:37 +0300

    tsconfig added. gzip and ungzip added.

--- ts/bad-apple.ts
+++ ts/bad-apple.ts
@@ -4,6 +4,7 @@ import {extactFrames} from "./extact-frames";
 import {uint8ToJimpArray} from "./uint8-to-jimp";
 import {compress} from "./compress";
 import {collectFrame} from "./collect-frame";
+import {gzip} from "node-gzip";
 
 export async function badApple(videoPath: string, outputFilePath: string, fps = 10): Promise<void> {
     const ffmpegInstance = createFFmpeg({log: true});
@@ -12,9 +13,10 @@ export async function badApple(videoPath: string, outputFilePath: string, fps = 
     const pages = await collectFrame(jimpArray);
     const joined = pages.join().split('\n');
     const compressed = compress(joined);
-    await writeFile(outputFilePath, compressed);
+    const gziped = await gzip(compressed);
+    await writeFile(outputFilePath, gziped);
 }
 
 (async _ => {
-    await badApple("vid/bad-apple.mp4", "./new_comp.txt")
+    await badApple("vid/bad-apple.mp4", "./bad-apple.rx");
 })();
--- ts/player.ts
+++ ts/player.ts
@@ -1,12 +1,21 @@
 import {uncompress} from "./uncompress";
 import {readFile} from "fs/promises";
+import {ungzip} from "node-gzip";
+import * as readline from 'node:readline/promises';
+import {stdin as input, stdout as output} from 'node:process';
+import {printFloppy} from "./floppy";
 
+const rl = readline.createInterface({input, output});
 const hideCursor = require("hide-terminal-cursor");
 
 export async function player(filePath: string, ms = 80) {
     hideCursor();
-    const compressed = await readFile(filePath, "utf-8");
-    const file = uncompress(compressed).split('~');
+    const gzRead = await readFile(filePath);
+    const compressed = await ungzip(gzRead);
+    const file = uncompress(compressed.toString()).split('~');
+    printFloppy();
+    await rl.question('Press Enter to Continue...');
+    rl.close();
     let index = 0;
     let iterator = setInterval(() => {
         console.clear();
@@ -17,4 +26,4 @@ export async function player(filePath: string, ms = 80) {
     }, ms);
 }
 
-player("./new_compress.txt");
+player("/Users/gokselkucuksahin/.files/bad-apple.rx");
