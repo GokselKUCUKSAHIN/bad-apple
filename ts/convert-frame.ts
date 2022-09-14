adcb188eab932fa49b17b7982bb61e751e961167
gokselkucuksahin <gokselkucuksahin@gmail.com>
2022-09-14 23:37:35 +0300

    Collect Frame and Convert Frame updated. Jimp raw image.data.buffer implemetend.

--- ts/convert-frame.ts
+++ ts/convert-frame.ts
@@ -29,3 +29,35 @@ export async function convertFrame(image: Jimp): Promise<string> {
         }
     });
 }
+
+
+export async function convertFrameBuffer(image: Jimp): Promise<string> {
+    // I'll return a result, I Promise!
+    return new Promise((resolve, reject) => {
+        let row = [];
+        try {
+            // Factory Pattern
+            image
+                .rgba(false)
+                .resize(100, 100)
+                .quality(60) // jpeg
+                .grayscale()
+                .contrast(1)
+                .posterize(2);
+            const len = image.bitmap.data.length
+            const w = image.bitmap.width;
+            // const h = image.bitmap.height;
+            for (let i = 0, idx = 0; i < len; i += 4, idx++) {
+                const dataR = image.bitmap.data[i];
+                row.push(dataR > 127 ? block : blank);
+                if (idx >= w) {
+                    idx = 0;
+                    row.push('\n');
+                }
+            }
+            resolve(row.join(''));
+        } catch (err) {
+            reject(err);
+        }
+    });
+}
